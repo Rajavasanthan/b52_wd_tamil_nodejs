c5a77b7d6ddeed1fcb4e8536d25e568f317d17c4
rajavasanthan <rajavasanthan@gmail.com>
2023-12-21 11:21:03 +0530

    register added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 const { MongoClient, ObjectId } = require("mongodb");
 const dotenv = require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const URL = process.env.DB;
 
 app.use(express.json());
@@ -12,46 +14,67 @@ app.use(
   })
 );
 
-let users = [];
+let students = [];
 
-app.get("/users", async (req, res) => {
+function authorize(req, res, next) {
+  console.log(req.headers);
+  if (req.headers.authorization) {
+    try {
+      const verify = jwt.verify(
+        req.headers.authorization,
+        process.env.SECRET_KEY
+      );
+      if (verify) {
+        next();
+      } else {
+        res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (error) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+app.get("/students",authorize, async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
     const db = connection.db("b52_wd_tamil");
-    const users = await db.collection("users").find().toArray();
+    const students = await db.collection("students").find().toArray();
     await connection.close();
-    res.json(users);
+    res.json(students);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
 
-app.post("/user", async (req, res) => {
+app.post("/student", authorize,async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
     const db = connection.db("b52_wd_tamil");
-    const user = await db.collection("users").insertOne(req.body);
+    const student = await db.collection("students").insertOne(req.body);
     await connection.close();
     res.json({ message: "User created successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
   }
-  // req.body.id = users.length + 1;
-  // users.push(req.body);
+  // req.body.id = students.length + 1;
+  // students.push(req.body);
   // res.json({ message: "User created successfully" });
 });
 
 // URL Params
-app.get("/user/:id", async (req, res) => {
+app.get("/student/:id",authorize, async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
     const db = connection.db("b52_wd_tamil");
-    const user = await db
-      .collection("users")
+    const student = await db
+      .collection("students")
       .findOne({ _id: new ObjectId(req.params.id) });
-    res.json(user);
+    res.json(student);
     await connection.close();
   } catch (error) {
     console.log(error);
@@ -59,21 +82,21 @@ app.get("/user/:id", async (req, res) => {
   }
 
   // let id = req.params.id;
-  // let user = users.find((user) => user.id == id);
+  // let student = students.find((student) => student.id == id);
 
-  // if (user) {
-  //   res.json(user);
+  // if (student) {
+  //   res.json(student);
   // } else {
   //   res.status(404).json({ message: "User not found" });
   // }
 });
 
-app.put("/user/:id", async (req, res) => {
+app.put("/student/:id",authorize, async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
     const db = connection.db("b52_wd_tamil");
-    const user = await db
-      .collection("users")
+    const student = await db
+      .collection("students")
       .findOneAndUpdate(
         { _id: new ObjectId(req.params.id) },
         { $set: req.body }
@@ -85,22 +108,22 @@ app.put("/user/:id", async (req, res) => {
     res.status(500).json({ message: "Something went worng" });
   }
 
-  // let index = users.findIndex((user) => user.id == req.params.id);
+  // let index = students.findIndex((student) => student.id == req.params.id);
   // if (index != -1) {
   //   req.body.id = parseInt(req.params.id);
-  //   users[index] = req.body;
+  //   students[index] = req.body;
   //   res.json({ message: "User updated successfully" });
   // } else {
   //   res.status(404).json({ message: "User not found" });
   // }
 });
 
-app.delete("/user/:id", async (req, res) => {
+app.delete("/student/:id",authorize, async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
     const db = connection.db("b52_wd_tamil");
-    const user = await db
-      .collection("users")
+    const student = await db
+      .collection("students")
       .deleteOne({ _id: new ObjectId(req.params.id) });
     await connection.close();
     res.json({ message: "User Deleted" });
@@ -108,13 +131,63 @@ app.delete("/user/:id", async (req, res) => {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
   }
-  // let index = users.findIndex((user) => user.id == req.params.id);
+  // let index = students.findIndex((student) => student.id == req.params.id);
   // if (index != -1) {
-  //   users.splice(index, 1);
+  //   students.splice(index, 1);
   //   res.json({ message: "User deleted successfully" });
   // } else {
   //   res.status(404).json({ message: "User not found" });
   // }
+});
+
+app.post("/register", async (req, res) => {
+  // email
+  // username
+  // password
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    req.body.password = hash;
+    const connection = await MongoClient.connect(URL);
+    const db = connection.db("b52_wd_tamil");
+    const user = await db.collection("user").insertOne(req.body);
+    await connection.close();
+    res.json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const connection = await MongoClient.connect(URL);
+    const db = connection.db("b52_wd_tamil");
+    const user = await db.collection("user").findOne({ email: req.body.email });
+    if (user) {
+      const password = await bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      console.log(password);
+      if (password) {
+        // JWT Token
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+          expiresIn: "1m",
+        });
+        res.json({ message: "Login success", token });
+      } else {
+        res.status(401).json({ message: "Username or Password is incorrect" });
+      }
+    } else {
+      res.status(404).json({ message: "Username or Password is incorrect" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 app.listen(3005);
